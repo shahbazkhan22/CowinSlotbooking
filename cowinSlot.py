d94fef792ce63f3c8092f585a7a5b2e82f654fc0
Shahbaz Khan <mdshahbaz_bt2k16@dtu.ac.in>
2021-05-12 16:05:17 +0530

    Update cowinSlot.py

--- cowinSlot.py
+++ cowinSlot.py
@@ -3,6 +3,7 @@ import json
 import time
 from datetime import datetime
 from playsound import playsound
+date = '14-05-2021'
 
 def check(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -18,7 +19,7 @@ def check(url):
 if __name__=="__main__":
     while(True):
         for i in range(140,151):
-            url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id='+str(i)+'&date=14-05-2021'
+            url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id='+str(i)+'&date='+date
             check(url)
         print('Sleeping for 30 secs',str(datetime.now().time()))
         time.sleep(30)
